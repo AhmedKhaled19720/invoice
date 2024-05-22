23eafabc6cd1c2224600c5e5ccf5868b1e404f69
Ahmed Khaled <ahmed.khaled19720gmail.com>
2024-05-22 16:38:17 +0300

    laguages(ar+en)

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,19 +1,22 @@
 <!-- Title -->
-<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+<title> Valex - Premium dashboard ui bootstrap rwd admin html5 template </title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:slnt,wght@-11..11,200..1000&display=swap" rel="stylesheet">
 <!-- Favicon -->
-<link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
+<link rel="icon" href="{{ URL::asset('assets/img/brand/favicon.png') }}" type="image/x-icon" />
 <!-- Icons css -->
-<link href="{{URL::asset('assets/css/icons.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css/icons.css') }}" rel="stylesheet">
 <!--  Custom Scroll bar-->
-<link href="{{URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<link href="{{ URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css') }}" rel="stylesheet" />
 <!--  Sidebar css -->
-<link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/plugins/sidebar/sidebar.css') }}" rel="stylesheet">
 <!-- Sidemenu css -->
-<link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
+<link rel="stylesheet" href="{{ URL::asset('assets/css-rtl/sidemenu.css') }}">
 @yield('css')
-<!--- Style css -->
-<link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
 <!--- Dark-mode css -->
-<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css-rtl/style-dark.css') }}" rel="stylesheet">
 <!---Skinmodes css-->
-<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+<link href="{{ URL::asset('assets/css-rtl/skin-modes.css') }}" rel="stylesheet">
+<!--- Style css -->
+<link href="{{ URL::asset('assets/css-rtl/style.css') }}" rel="stylesheet">
